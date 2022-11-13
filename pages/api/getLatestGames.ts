a0e81c8269ebefccacb04ef21e5b9881afbf0aad
Patrick McCoy <pmccoy@gannett.com>
2022-11-13 12:26:40 -0600

    thin setup

---
+++ pages/api/getLatestGames.ts
@@ -0,0 +1,17 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getCurrentUser } from "thin-backend";
+import { bggAPIService } from "~/services/bggAPIService";
+
+const bggAPIServiceInstance = bggAPIService()
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+	const currentUser = await getCurrentUser()
+	await bggAPIServiceInstance.getUserGamesOwned(currentUser?.bggUsername || 'baldgoat')
+		.then((result) => {
+			//TODO add error handling
+			res.status(200).json({status: 'ok'})
+		})
+}
