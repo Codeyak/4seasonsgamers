cf90d58a2fe7240d05bd7ad1ef306358cfdbf001
Patrick McCoy <pmccoy@gannett.com>
2022-11-13 12:26:07 -0600

    thin setup

--- services/bggAPIService.ts
+++ services/bggAPIService.ts
@@ -1,5 +1,6 @@
 import got, { RequestError } from 'got'
-import { parseString } from 'xml2js'
+import { createRecords } from 'thin-backend'
+import xlm2js from 'xml2js'
 
 export interface IbggAPIService {
 	getUserGamesOwned: (username: string) => Promise<unknown>
@@ -13,13 +14,38 @@ export const bggAPIService = ():IbggAPIService => {
 
 		return got.get(requestUrl)
 			.then((result) => {
+				const parser = new xlm2js.Parser()
 				const body = result.body
-				const collection = parseString(body, (err, result) => {
-					console.log('Collection', result.items.item)
-					return result
+				return parser.parseStringPromise(body)
+					.then(async (result) => {
+						const bggItems = result.items.item
+						console.log(bggItems)
+						const userGames = bggItems.map((bggGame) => {
+							return {
+								id: parseInt(bggGame['$'].objectid),
+								name: bggGame.name[0]._,
+								image: bggGame.image[0],
+								thumbnail: bggGame.thumbnail[0]
+							}
+						})
+						try {
+							// const gamesCreated = await createRecords('games', userGames)
+						} catch (error) {
+							console.log('ERROR', error)
+						}
+						return true
 				})
-				return collection
 		})
+	}
+
+	const getGameData = async (id:number) => {
+		//TODO get full data for a game
+	}
+
+	const _parseXML = async (body:string) => {
+		const parser = new xlm2js.Parser()
+		const json = await parser.parseString(body)
+		return json
 	}
 
 	return {
