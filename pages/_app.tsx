6f66d07b64ff235aa2d9da8df0d149cb9298403a
Patrick McCoy <pmccoy@gannett.com>
2023-01-01 11:53:00 -0600

    sets up mui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,57 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { CssBaseline } from '@mui/material'
+import { ThemeProvider } from '@mui/material/styles'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import Image from 'next/image'
+import theme from '~/styles/theme'
+import createEmotionCache from '~/utilities/createEmotionCache'
+import { CacheProvider } from '@emotion/react'
+import { EmotionCache } from '@emotion/react'
 import { Provider as StoreProvider } from 'react-redux'
 import { store } from '~/store'
 
-function MyApp({ Component, pageProps }: AppProps) {
+const clientSideEmotionCache = createEmotionCache()
+
+interface IProps extends AppProps {
+	emotionCache: EmotionCache
+}
+
+const MyApp = ({ Component, emotionCache = clientSideEmotionCache, pageProps }: IProps): JSX.Element => {
+
 	return (
-		<StoreProvider store={store}>
-			<Component {...pageProps} />
-		</StoreProvider>
+		<>
+			<Head>
+				<title>4 Seasons Gamers</title>
+				<link
+					rel="icon"
+					href="./favicon.ico"
+				/>
+				<meta
+					name="viewport"
+					content="minimum-scale=1, initial-scale=1, width=device-width"
+				/>
+			</Head>
+			<StoreProvider store={store}>
+				<CacheProvider value={ emotionCache }>
+					<ThemeProvider theme={theme}>
+							<CssBaseline />
+							<Component {...pageProps} />
+					</ThemeProvider>
+				</CacheProvider>
+			</StoreProvider>
+			<footer>
+				<a
+					href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Powered by{' '}
+					<span>
+						<Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+					</span>
+				</a>
+			</footer>
+		</>
 	)
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchGames, selectedGames } from '~/store/gamesSlice'
@@ -38,29 +37,12 @@ const Home: NextPage = () => {
 
 
 	return (
-		<div className={styles.container}>
-			<Head>
-				<title>4 Seasons Gamers</title>
-				<meta name="description" content="Gaming for All Seasons" />
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-
-			<main className={styles.main}>
-				<h1 className={styles.title}>Welcome to 4 Seasons Gamers!</h1>
+		<div>
+			<main>
+				<h1>Welcome to 4 Seasons Gamers!</h1>
 			</main>
 
-			<footer className={styles.footer}>
-				<a
-					href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Powered by{' '}
-					<span className={styles.logo}>
-						<Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-					</span>
-				</a>
-			</footer>
+
 		</div>
 	)
 }
