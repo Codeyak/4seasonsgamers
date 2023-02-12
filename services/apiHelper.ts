c5cc1ec1692fe50747f0e469f6dacb8b2a2be4af
Patrick McCoy <pmccoy@gannett.com>
2023-02-12 14:58:24 -0600

    fix apiHelper type error

--- services/apiHelper.ts
+++ services/apiHelper.ts
@@ -3,11 +3,11 @@ import { NextApiRequest } from 'next'
 export const stringFromParameter = (req: NextApiRequest, key: string):string => {
 	const item = req.query[key]
 	if (Array.isArray(item)) { return item[0] }
-	return item
+	return String(item)
 }
 
 export const arrayFromParameter = (req: NextApiRequest, key: string):string[] => {
 	const item = req.query[key]
 	if (Array.isArray(item)) { return item }
-	return [ item ]
+	return [ String(item) ]
 }
